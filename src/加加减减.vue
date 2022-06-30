c9e24681e4e309500db701b4b3b13d003f2af696
TTpeach1 <TTpeach1@Outlook.com>
2022-06-30 20:56:03 +0800

    购物车

---
+++ src/加加减减.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <ul>
+      <li v-for="(item,index) in arr" :key="item.index">
+        <span>{{item}}</span>
+        <button @click="del(index)">删除</button>
+      </li>
+    </ul>
+    <button @click="addFn">生成</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      arr:[1,5,3]
+    };
+  },
+  methods: {
+    addFn(){
+      this.arr.push(Math.floor(Math.random() * 10))
+    },
+    del(index){
+      this.arr.splice(index, 1);
+    }
+  },
+};
+</script>
